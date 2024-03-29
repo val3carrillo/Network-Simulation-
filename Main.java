22e9254c86d2a43cd55afbe966c94010dcaf9094
Bet <elvisramirez999@gmail.com>
2024-03-29 03:51:09 +0000

    Fixed when source does not equal firstHost

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ import java.io.FileReader;
 
 public class Main {
     public static void main(String[] args) {
-        String fileName = "simulation1.txt";
+        String fileName = "simulation4.txt";
 
         try(BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             int firstHostAddress = Integer.parseInt(br.readLine());
@@ -27,6 +27,7 @@ public class Main {
             }
 
             while((line = br.readLine()) != null ){
+                System.out.println("Reading line: " + line);
                 String[] parts = line.split(" ");
                 int sourceAddr = Integer.parseInt(parts[0]);
                 int destAddr = Integer.parseInt(parts[1]);
@@ -37,10 +38,19 @@ public class Main {
                 System.out.println("Destination Address: " + destAddr);
                 System.out.println("Interval " + interval);
                 System.out.println("Duration " + duration);
-
-                if(sourceAddr == firstHostAddress){
-                    ((SimpleHost) firstHost).sendPings(destAddr, interval, duration);
+                // Here it is checking if the host address is the same as the first host address
+                if (sourceAddr == firstHostAddress) {
+                ((SimpleHost) firstHost).sendPings(destAddr, interval, duration);
                 }
+                // Here it creating a temporary host object and adding the firstHost as a neighbor and sending pings
+                // Now the only problem is adding all events together and printing them like the pdf shows
+                else {
+                    Host tempHost = new SimpleHost(sourceAddr, interval, duration);
+                    tempHost.setHostParameters(sourceAddr, fel);
+                    tempHost.addNeighbor(firstHost, Math.abs(tempHost.getHostAddress() - firstHostAddress));
+                    ((SimpleHost) tempHost).sendPings(destAddr, interval, duration);
+                }
+                
             }
 
             Event event;
